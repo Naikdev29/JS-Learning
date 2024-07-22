c10db5d76bc90db5dfc523ca3b2b238456f965ca
Naikdev29 <devnaik75@gmail.com>
2024-07-22 01:02:52 -0400

    Js Basic Learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountNumber = 123;
+let accountEmail = "dev@gmail.com";
+var accountpassword = "234";
+accountCity = "Surat";
+
+accountEmail = "naik@gmail.com"
+accountpassword = "456"
+accountCity = "thane"
+
+/*
+do not use var
+*/
+
+console.table([accountNumber, accountEmail, accountpassword, accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+let name = "Dev";
+let age = 19
+let isStraight = true;
+
+// datatypes
+// numbers
+// straing => "" `` ''
+// bigInt
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined

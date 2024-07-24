2e318aa55111590e4dba1c519ff14492f4153e24
Naikdev29 <devnaik75@gmail.com>
2024-07-24 01:19:08 -0400

    More Learing in Datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+// Datatypes
+
+// Primitive
+
+// 7 types : String, Number, null, Boolean, undefined, symbol, BigInt
+
+// Refernce(Non Primitive)
+
+// Array, objects, Functions
+
+let WCs = [1983, 2007, 2011, 2013, 2024]
+
+let captains =  {
+    name : "Kapil Dev",
+    name1: "Dhoni",
+    name2: "Rohit"
+}
+
+let myFunctions = function(){
+     console.log("Hello Dev");
+}
+
+
+// **************************** Memorys in JS **************************************************
+
+// Stack (Primitive), Heap (Non Primitive)
+
+

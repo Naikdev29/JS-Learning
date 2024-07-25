9bd3c94a6f408af57eda270a0d5c8aa2e7b3bac7
Naikdev29 <devnaik75@gmail.com>
2024-07-25 01:03:38 -0400

    more learning

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name = "Dev"
+const bodyCount = 0
+
+
+console.log(`hello my name is ${name} and my body count is ${bodyCount}`);
+
+
+const name1 = "Dev-Naik"
+const name2 = "    Dev Naik          "
+const name3 = "DEV-NAIK-ASHESHBHAI"
+console.log(name1[0]);
+console.log(name1.length);
+console.log(name1.toUpperCase());
+console.log(name1.charAt('4'));
+console.log(name1.indexOf('N'));
+console.log(name1.substring(0, 4));
+console.log(name1.slice(-8, 2));
+console.log(name2.trim());
+console.log(name2.replace('Dev','ved').trim());
+console.log(name3.split('-'));
